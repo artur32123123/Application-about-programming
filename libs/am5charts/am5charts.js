5416e7890c6f3928dccd0cd7a630a11541a8beff
Артур <ffilosssoff@gmail.com>
2024-12-20 02:41:09 +0300

    add Выполнена верстка шапки сайта

---
+++ libs/am5charts/am5charts.js
@@ -0,0 +1,4 @@
+import am5charts from '@amcharts/amcharts5-charts';
+import am5 from '@amcharts/amcharts5';
+import am5themes_Animated from '@amcharts/amcharts5/themes/Animated';
+import am5themes_Dark from '@amcharts/amcharts5/themes/Dark';
